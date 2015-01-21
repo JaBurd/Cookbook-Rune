227101532aeedb62a28ae67189c2078402dac5dd
502375908 <ian.henry@ge.com>
2015-01-21 10:34:45 -0500

    include package install logic

--- resources/package.rb
+++ resources/package.rb
@@ -15,3 +15,4 @@ attribute :endpoint, :kind_of => String
 attribute :checksum, :kind_of => String
 attribute :owner, :kind_of    => String, :regex => Chef::Config[:user_valid_regex], :default => 'root'
 attribute :group, :kind_of    => String, :regex => Chef::Config[:user_valid_regex], :default => 'root'
+attribute :file_name, kind_of: String, required: true
