7974025550acf8deb9593c44225039245ef977c5
502375908 <ian.henry@ge.com>
2015-01-26 14:47:46 -0500

    refactored

--- providers/upload.rb
+++ providers/upload.rb
@@ -1,26 +1,21 @@
 #
 # Cookbook Name:: Rune
-# Provider:: deploy
+# Provider:: config
 #
 # Author:: Ian Henry (<ianjhenry00@gmail.com>)
 #
 # Copyright 2015
 #
-require 'chef/provider/lwrp_base'
 require_relative '../libraries/helpers'
+include Rune::Helpers
 
-#class Chef
-#  class Provider
-#    class RuneUpload < Chef::Provider::LWRPBase
-      include Rune::Helpers
+  def whyrun_supported?
+    true
+  end
 
-      def whyrun_supported?
-        true
-      end
-
-      action :upload do
-        converge_by("Upload #{ @new_resource.artifact }") do
-          # Upload artifact from helpers.rb
-          upload_rune_artifact
-        end
-      end
+  action :upload do
+    converge_by("Upload #{ @new_resource.artifact }") do
+      # Upload artifact from helpers.rb
+      rune_artifact_upload
+    end
+  end
--- resources/deploy.rb
+++ resources/deploy.rb
@@ -1,20 +1,18 @@
+#
+# Cookbook Name:: rune
+# Resource:: deploy
+#
+# Author:: Ian Henry (<ianjhenry00@gmail.com>)
+#
+# Copyright 2015
+#
 require 'chef/resource/lwrp_base'
 
-#class Chef
-#  class Resource
-#    class RuneDeploy < Chef::Resource::LWRPBase
-#      self.resource_name = :rune_deploy
-      actions :deploy, :delete, :nothing
-      default_action :deploy
+actions :deploy, :nothing
+default_action :deploy
 
-      attribute :artifact, kind_of: String, name_attribute: true, required: true
-      attribute :target, kind_of: String
-      attribute :endpoint, kind_of: String, required: true
-      attribute :repo, kind_of: String, required: true
-      attribute :filename, kind_of: String
-
-#      include Rune::Helpers
-
-#    end
-#  end
-#end
+attribute :artifact, kind_of: String, name_attribute: true, required: true
+attribute :target, kind_of: String
+attribute :endpoint, kind_of: String, required: true
+attribute :repo, kind_of: String, required: true
+attribute :filename, kind_of: String
