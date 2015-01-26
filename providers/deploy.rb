7a5eadacaa281fd294b2ef55dcc98de34b3e3186
502375908 <ian.henry@ge.com>
2015-01-26 13:37:51 -0500

    corrected deploy code, first functioning build for config and deploy

--- providers/deploy.rb
+++ providers/deploy.rb
@@ -6,37 +6,26 @@
 #
 # Copyright 2015
 #
-require 'digest'
-require 'pathname'
-require 'yaml'
-require 'chef/provider/lwrp_base'
 require_relative '../libraries/helpers'
+include Rune::Helpers
 
-#class Chef
-#  class Provider
-#    class RuneDeploy < Chef::Provider::LWRPBase
-      include Rune::Helpers
+  def whyrun_supported?
+    true
+  end
 
-      def whyrun_supported?
-        true
-      end
-
-     action :deploy do
-       converge_by("Deploy #{ @new_resource.artifact }") do
-       # Deploy artifact from helpers.rb
-         deploy_rune_artifact
-       end
-     end
-
-    action :delete do
-      converge_by("Delete #{ @new_resource.artifact}") do
-      # Delete artifact in artifactory from helpers.rb
-        delete_rune_artifact
-      end
+  action :deploy do
+    converge_by("Deploy #{ @new_resource.artifact }") do
+  # Deploy artifact from helpers.rb
+      deploy_rune_artifact
     end
-#  end
-#  end
-#end
+  end
+
+  action :delete do
+    converge_by("Delete #{ @new_resource.artifact}") do
+      # Delete artifact in artifactory from helpers.rb
+      delete_rune_artifact
+    end
+  end
 
 
 
